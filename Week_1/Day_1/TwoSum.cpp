1edc2ba49bcb398b57100b4936c361d008fabf45
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-02 17:43:32 +0530

    Update TwoSum.cpp

--- Week_1/Day_1/TwoSum.cpp
+++ Week_1/Day_1/TwoSum.cpp
@@ -1,1 +1,15 @@
-day_1 solution
+day_1 solutionclass Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+       int element=target-nums[i];
+        if(mp.find(element)!=mp.end())
+        return {mp[element],i};
+        
+        mp[nums[i]]=i;
+        }
+         return {};
+    }
+   
+};
