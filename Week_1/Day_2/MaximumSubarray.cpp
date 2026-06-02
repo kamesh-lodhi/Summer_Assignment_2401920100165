66511bb56b10a8a43be092ee6ba12c56eabc6418
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-02 22:27:17 +0530

    Update MaximumSubarray.cpp

--- Week_1/Day_2/MaximumSubarray.cpp
+++ Week_1/Day_2/MaximumSubarray.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+//kadane mein -ve ke liye work nhi karegi
+//this is optimal for max subarray yaad rakhio
+    int maxSubArray(vector<int>& nums) {
+       int maxsub=nums[0],currsum=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(currsum+nums[i]>nums[i]){
+                currsum=currsum+nums[i];
+            }
+            else{
+                currsum=nums[i];
+            }
+            maxsub=max(maxsub,currsum);
+        }
+        return maxsub ;
+    }
+};
