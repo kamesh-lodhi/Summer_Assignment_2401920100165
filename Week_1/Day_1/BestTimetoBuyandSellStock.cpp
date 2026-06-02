8c0c341a4639ab2a4630a9116d7cb2447e852b26
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-02 22:18:46 +0530

    Update BestTimetoBuyandSellStock.cpp

--- Week_1/Day_1/BestTimetoBuyandSellStock.cpp
+++ Week_1/Day_1/BestTimetoBuyandSellStock.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int max_profit=0;
+        int cost;
+        for(int i=1;i<prices.size();i++){
+            cost=prices[i]-mini;
+            max_profit=max(cost,max_profit);
+            mini=min(mini,prices[i]);
+        }
+        return max_profit;
+    }
+};
