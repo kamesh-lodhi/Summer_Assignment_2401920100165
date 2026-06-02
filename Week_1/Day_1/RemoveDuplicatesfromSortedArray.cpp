b383c92f01acae1142e540f033038c39c1ba8077
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-02 22:15:36 +0530

    Update RemoveDuplicatesfromSortedArray.cpp

--- Week_1/Day_1/RemoveDuplicatesfromSortedArray.cpp
+++ Week_1/Day_1/RemoveDuplicatesfromSortedArray.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+     int i=0;
+     int n=nums.size();
 
+     int count =0;
+     for(int j=1;j<n;j++){
+        while(j<n &&nums[i]==nums[j])
+        j++;
+        if (j < n) {
+                i++;
+                nums[i] = nums[j];
+            }
+        ;
+
+     }
+     return i+1;
+    }
+};
