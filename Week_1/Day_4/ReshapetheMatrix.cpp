0c8246efdf765ceb3c2c676a924b890d5c722e2e
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-04 14:05:40 +0530

    Update ReshapetheMatrix.cpp

--- Week_1/Day_4/ReshapetheMatrix.cpp
+++ Week_1/Day_4/ReshapetheMatrix.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        
 
+        vector<vector<int>>ans;
+        vector<int>row;
+        int n=mat.size();
+        int m=mat[0].size();
+        if (n * m != r * c)
+            return mat;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                row.push_back(mat[i][j]);
+                 if(row.size()==c)
+            {
+                ans.push_back(row);
+                row.clear();
+            }
+            }
+           
+        }
+        return ans;
+    }
+};
