f39f2899373665266aa1504f5ba3936655cfbba4
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-05 17:21:37 +0530

    Update SpiralMatrix.cpp

--- Week_1/Day_4/SpiralMatrix.cpp
+++ Week_1/Day_4/SpiralMatrix.cpp
@@ -1,32 +1,41 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) return {}; 
 
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        vector<int>arr(n);
-        for(int i=0;i<n;i++)
-        {
-            cin>>arr[i];
-        }
-        for(int i=0;i<n;i++){
-            if(arr[i]==1)
-            arr[i]++;
-        }
-        for(int i=0;i<n-1;i++)
-        {
-            if(arr[i+1]%arr[i]==0){
-                arr[i+1]++;
+        int l = 0, r = matrix[0].size() - 1;
+        int top = 0, down = matrix.size() - 1;
+        vector<int> ans;
+
+        while (l <= r && top <= down) {
+            // Traverse top row
+            for (int column = l; column <= r; column++) {
+                ans.push_back(matrix[top][column]);
+            }
+            top++;
+
+            // Traverse right column
+            for (int row = top; row <= down; row++) {
+                ans.push_back(matrix[row][r]);
+            }
+            r--;
+
+            // Traverse bottom row
+            if (top <= down) {
+                for (int column = r; column >= l; column--) {
+                    ans.push_back(matrix[down][column]);
+                }
+                down--;
+            }
+
+            // Traverse left column
+            if (l <= r) {
+                for (int row = down; row >= top; row--) {
+                    ans.push_back(matrix[row][l]);
+                }
+                l++;
             }
         }
-       for(int itr:arr){
-       cout<<itr<<" ";
-       }
-       cout<<endl;
+        return ans;
     }
-    return 0;
-}
+};
