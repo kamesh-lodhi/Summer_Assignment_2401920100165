f67c3f0cb29c85527805a34d1e01e6a92816d9da
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-05 17:38:58 +0530

    Update ReverseString.cpp

--- Week_1/Day_5/ReverseString.cpp
+++ Week_1/Day_5/ReverseString.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;// or swapChar(s[i],s[j])
+            i++;
+            j--;
+        }
+       
+       
+    }
+};
