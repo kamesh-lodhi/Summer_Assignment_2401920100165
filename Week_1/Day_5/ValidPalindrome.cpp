97a020ea67377b9505ca3c691000682227a216b6
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-05 17:34:46 +0530

    Update ValidPalindrome.cpp

--- Week_1/Day_5/ValidPalindrome.cpp
+++ Week_1/Day_5/ValidPalindrome.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        //issalnum and tolower
+        int i=0;
+        int j=s.size()-1;
+        while(i<j)
+        {
+            if(isalnum(s[i])&&isalnum(s[j]))
+            {
+                if(tolower(s[i])==tolower(s[j]))
+                {
+                    i++;
+                    j--;
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(!(isalnum(s[i])))
+            i++;
+            else
+            j--;
+        }
+        return true;
+    }
+};
