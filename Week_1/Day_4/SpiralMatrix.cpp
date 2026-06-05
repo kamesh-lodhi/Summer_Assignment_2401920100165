efc7127e0912dfc9563dd33429046b001e5a4539
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-05 17:21:17 +0530

    Update SpiralMatrix.cpp

--- Week_1/Day_4/SpiralMatrix.cpp
+++ Week_1/Day_4/SpiralMatrix.cpp
@@ -1,1 +1,32 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]==1)
+            arr[i]++;
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i+1]%arr[i]==0){
+                arr[i+1]++;
+            }
+        }
+       for(int itr:arr){
+       cout<<itr<<" ";
+       }
+       cout<<endl;
+    }
+    return 0;
+}
