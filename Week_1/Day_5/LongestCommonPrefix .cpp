ee5e76c1d1c603ac3e35d3b3c4064f48d4d12b87
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-05 18:02:59 +0530

    Update LongestCommonPrefix .cpp

--- Week_1/Day_5/LongestCommonPrefix .cpp
+++ Week_1/Day_5/LongestCommonPrefix .cpp
@@ -1,1 +1,21 @@
 
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+      string ans="";
+      int i=0;
+      while(i<strs[0].size()){
+        char ch=strs[0][i];
+      for(int j=1;j<strs.size();j++){
+        if(i >= strs[j].size()||ch!=strs[j][i] )
+        {
+            return ans;
+        }
+      }
+        ans+=ch;
+        i++; 
+      }
+      
+      return ans;
+    }
+};
