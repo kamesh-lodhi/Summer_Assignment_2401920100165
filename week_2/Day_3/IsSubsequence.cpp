1a0c6bf3aac2269c31811700cfec92c250be56ef
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-17 14:09:02 +0530

    Update IsSubsequence.cpp

--- week_2/Day_3/IsSubsequence.cpp
+++ week_2/Day_3/IsSubsequence.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        int j=0;
+        int n=s.size();
+        int m=t.size();
+        while(j<m&&i<n ){
+            if(s[i]==t[j])
+            {
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return i==n;
+    }
+};
