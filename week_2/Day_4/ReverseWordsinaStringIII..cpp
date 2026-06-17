19dc2fed29c15ed0e690cccfd5a9564a53707ccb
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-17 15:29:04 +0530

    Update ReverseWordsinaStringIII..cpp

--- week_2/Day_4/ReverseWordsinaStringIII..cpp
+++ week_2/Day_4/ReverseWordsinaStringIII..cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+void add(string &ans,string word){
+    string rev="";
+    for(int i=0;i<word.size();i++){
+        rev=word[i]+rev;
+    }
+    ans=ans+rev+" ";
+}
+    string reverseWords(string s) {
+        string word;
+        int n=s.size();
+        string ans="";
+        stringstream ss(s);
+        while(ss>>word){
+        add(ans,word);
+        }
+        
+        return ans.substr(0,n);
+    }
+};
