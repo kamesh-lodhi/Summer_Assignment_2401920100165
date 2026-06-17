1460ccc51086b3d5e726e4c329bf6a6730b8222e
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-17 14:05:39 +0530

    Update ImplementstrStr().cpp

--- week_2/Day_3/ImplementstrStr().cpp
+++ week_2/Day_3/ImplementstrStr().cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+         if (needle.empty()) return 0;
 
+        int n = haystack.size();
+        int m = needle.size();
+
+        for(int i=0;i<=n-m;i++){
+           string occur=haystack.substr(i,m);
+           if(occur==needle)
+           return i;
+        }
+        return -1;
+    }
+};
