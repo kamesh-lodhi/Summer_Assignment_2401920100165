91c8615973552ef875db778004c5edace8996c12
kamesh lodhi <kameshlodhi3090@gmail.com>
2026-06-17 17:17:26 +0530

    Update GenerateParentheses.cpp

--- week_2/Day_4/GenerateParentheses.cpp
+++ week_2/Day_4/GenerateParentheses.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+    void find(int n,int l,int r,vector<string>&ans,string &temp){
+        if(l+r==2*n)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        if(l<n){
+            temp.push_back('(');
+            find(n,l+1,r,ans,temp);
+            temp.pop_back();
+        }
+        if(r<l){
+            temp.push_back(')');
+            find(n,l,r+1,ans,temp);
+            temp.pop_back();
+        }
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        string temp;
+        int l=0,r=0;
+        find(n,l,r,ans,temp);
+        return ans;
+    }
+};
